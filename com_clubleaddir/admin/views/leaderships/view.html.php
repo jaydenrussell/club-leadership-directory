7acd2f0b3722ad0d3432b829cb220df6f1fb7cfc
Jayden Russell <jayden.russell@users.noreply.github.com>
2026-08-19 03:53:24 -0400

    Fix 'Call to undefined method ToolbarHelper::saveorder()' (v2.0.12): that method does not exist in Joomla 3.10 (it is J4+). Replaced with ToolbarHelper::custom('leadership.saveorder', ...) which is the native J3 way to add a Save Order button that submits the order[] inputs.

--- com_clubleaddir/admin/views/leaderships/view.html.php
+++ com_clubleaddir/admin/views/leaderships/view.html.php
@@ -58,7 +58,9 @@ class ClubleaddirViewLeaderships extends HtmlView
         if ($canDo->get('core.edit.state')) {
             ToolbarHelper::publish('leadership.publish', 'JTOOLBAR_PUBLISH', true);
             ToolbarHelper::unpublish('leadership.publish', 'JTOOLBAR_UNPUBLISH', true);
-            ToolbarHelper::saveorder('leadership.saveorder', 'JTOOLBAR_SAVE_ORDER');
+            // Joomla 3.10 has no ToolbarHelper::saveorder(); use a custom button
+            // that submits the order[] inputs via the leadership.saveorder task.
+            ToolbarHelper::custom('leadership.saveorder', 'icon-menu', '', 'JTOOLBAR_SAVE_ORDER', false);
         }
         if ($canDo->get('core.delete')) {
             ToolbarHelper::deleteList('JGLOBAL_CONFIRM_DELETE', 'leadership.delete', 'JTOOLBAR_DELETE');
