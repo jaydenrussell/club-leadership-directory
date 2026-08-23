bb8f5141a503012d1e50dbffe155d947e0257361
Jayden Russell <jayden.russell@users.noreply.github.com>
2026-08-23 04:39:23 -0400

    Fix: child zips have manifest at zip root; fixes JInstaller setup-file error

--- com_clubleaddir/admin/controller.php
+++ com_clubleaddir/admin/controller.php
@@ -13,25 +13,4 @@ use Joomla\CMS\MVC\Controller\BaseController;
 class ClubleaddirController extends BaseController
 {
 	protected $default_view = 'leaderships';
-
-	public function save($key = null, $urlVar = null)
-	{
-		$model = $this->getModel('config');
-		$form = $model->getForm();
-		$data = $this->input->post->get('jform', array(), 'array');
-
-		if (!$model->save($data))
-		{
-			$this->setError($model->getError());
-			$this->setMessage($this->getError(), 'error');
-
-			$view = $this->getView('config', 'html');
-			$view->setModel($model, true);
-			$view->display();
-			return false;
-		}
-
-		$this->setMessage(JText::_('COM_CLUBLEADDIR_CONFIG_SAVED'));
-		$this->setRedirect(JRoute::_('index.php?option=com_clubleaddir&view=config', false));
-	}
 }
